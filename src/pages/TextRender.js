b301a96d0696eabbd0eaa531558bc213b25b536b
proaxius <102849799+proaxius@users.noreply.github.com>
2023-11-03 11:42:32 +0530

    new structure defined and nextjs used

---
+++ src/pages/TextRender.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TextRender(props) {
+ 
+  return (
+    <div>
+    <span>{props.quote}</span></div>
+  )
+}
---
+++ src/pages/authorRender.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function AuthorRender(props) {
+  return (
+    <div>{props.author}</div>
+  )
+}
